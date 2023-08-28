27039d4881193dc7442619bcbf651ec90c8cdca2
5yxar <5yx4r@mail.ru>
2023-08-28 21:32:45 +0300

    new bot

---
+++ ConsoleApp3/Shared/BaggageType.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp3.Shared;
+
+public class BaggageType
+{
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public object Description { get; set; }
+    public object CarBaggageInfo { get; set; }
+}
---
+++ ConsoleApp3/Shared/Discount.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp3.Shared;
+
+public class Discount
+{
+    public string DiscountType { get; set; }
+    public string Description { get; set; }
+}
